fbf72187385f0a4f06d1795141f5e9fa9a1f067a
Pcjmy <1716045191@qq.com>
2023-03-14 15:28:40 +0800

    feat: 列表渲染

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+    </ul>
+    <button @click="change">点击</button>
   </div>
 </template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator'
+
+interface Item {
+  id: number;
+  name: string;
+}
+
+export default Vue.extend({
+  data() {
+    return {
+      items: [
+        { id: 1, name: "Apple" },
+        { id: 2, name: "Banana" },
+        { id: 3, name: "Cherry" },
+      ] as Item[]
+    }
+  },
+  methods: {
+    change() {
+      this.items[0].name = 'Orange';
+    }
+  }
+})
+</script>
+
+<style>
+ul {
+  width: 50px;
+}
+</style>
