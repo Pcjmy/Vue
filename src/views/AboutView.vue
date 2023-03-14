85201ddc1867723b6db6af2085fb9e7b7b4f4683
Pcjmy <1716045191@qq.com>
2023-03-14 22:32:45 +0800

    feat: 列表渲染

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul>
+      <li v-for="(item,index) in items" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="change">点击</button>
   </div>
 </template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+  name: 'AboutView',
+  setup () {
+    const items = reactive(['Apple', 'Banana', 'Cherry'])
+    function change () {
+      items[0] = 'Orange'
+    }
+    return { items, change }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  width: 100px;
+}
+</style>
