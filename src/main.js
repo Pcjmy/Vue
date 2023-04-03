5d6dd27a63e4645d5a8ec0bbfc0c62993c14b65c
Pcjmy <1716045191@qq.com>
2023-04-03 20:17:23 +0800

    feat: event-emitter

--- src/main.js
+++ src/main.js
@@ -2,19 +2,39 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import mitt from 'mitt'
+// import mitt from 'mitt'
 
-const emitter = mitt() // 单例
+// const emitter = mitt() // 单例
 
-emitter.on('change', () => {
-  console.log('change1')
+// emitter.on('change', () => {
+//   console.log('change1')
+// })
+
+// emitter.on('change', () => {
+//   console.log('change2')
+// })
+
+// emitter.emit('change')
+// emitter.emit('change')
+// emitter.emit('change')
+
+import eventEmitter from 'event-emitter'
+
+const emitter = eventEmitter()
+
+emitter.on('change', (value) => {
+  console.log('change1', value)
 })
 
 emitter.on('change', () => {
   console.log('change2')
 })
 
-emitter.emit('change')
+emitter.once('change', () => {
+  console.log('change3')
+})
+
+emitter.emit('change', 'abc')
 emitter.emit('change')
 emitter.emit('change')
 
