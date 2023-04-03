54cc284623131b48316a865d581d0f5bd86b43c6
Pcjmy <1716045191@qq.com>
2023-04-03 20:06:01 +0800

    feat: mitt

--- src/main.js
+++ src/main.js
@@ -2,5 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import mitt from 'mitt'
+
+const emitter = mitt() // 单例
+
+emitter.on('change', () => {
+  console.log('change1')
+})
+
+emitter.on('change', () => {
+  console.log('change2')
+})
+
+emitter.emit('change')
+emitter.emit('change')
+emitter.emit('change')
 
 createApp(App).use(store).use(router).mount('#app')
