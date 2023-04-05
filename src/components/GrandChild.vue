b0410f19f5a5f7d51ac27b425ceb1f988d0d752a
Pcjmy <1716045191@qq.com>
2023-04-05 23:30:58 +0800

    feat: provide和inject

---
+++ src/components/GrandChild.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>
+    Message to grand child: {{ message }}
+  </p>
+</template>
+
+<script setup>
+import { inject } from 'vue'
+
+const message = inject('message')
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,34 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <button id="show-modal" @click="showModal = true">Show Modal</button>
-    <Teleport to="body">
-      <!-- 使用这个 modal 组件，传入 prop -->
-      <modal :show="showModal" @close="showModal = false">
-        <template #header>
-          <h3>custom header</h3>
-        </template>
-      </modal>
-    </Teleport>
+    <input v-model="message">
+    <Child />
   </div>
 </template>
 
-<script>
-import { ref } from 'vue'
-import Modal from '../components/Modal'
+<script setup>
+import { ref, provide } from 'vue'
+import Child from '../components/Child'
 
-export default {
-  name: 'AboutView',
-  components: { Modal },
-  setup () {
-    const showModal = ref(false)
-    return { showModal }
-  }
-}
+// by providing a ref, the GrandChild
+// can react to changes happening here.
+const message = ref('hello')
+provide('message', message)
 </script>
-
-<style scoped>
-ul {
-  width: 100px;
-}
-</style>
